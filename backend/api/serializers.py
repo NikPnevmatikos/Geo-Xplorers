3f49c8bb53d96e5ebd1fa71edabd322f94f6102a
JohnKalesis1 <sdi1900069@di.uoa.gr>
2023-05-28 18:50:54 +0300

    Working user endpoints

---
+++ backend/api/serializers.py
@@ -0,0 +1,143 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import *
+from rest_framework import status, exceptions
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenObtainSerializer
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+
+# class MyTokenObtainPairSerializer(TokenObtainPairSerializer, TokenObtainSerializer):
+    
+  
+#     # Overiding validate function in the TokenObtainSerializer  
+#     def validate(self, attrs):
+#         authenticate_kwargs = {
+#             'username': attrs['username'],
+#             'password': attrs['password'],
+#         }
+#         try:
+#             authenticate_kwargs['request'] = self.context['request']
+#         except KeyError:
+#             pass
+
+#         # print(f"\nthis is the user of authenticate_kwargs {authenticate_kwargs['email']}\n")
+       
+        
+#         '''
+#         Checking if the user exists by getting the email(username field) from authentication_kwargs.
+#         If the user exists we check if the user account is active.
+#         If the user account is not active we raise the exception and pass the message. 
+#         Thus stopping the user from getting authenticated altogether. 
+        
+#         And if the user does not exist at all we raise an exception with a different error message.
+#         Thus stopping the execution righ there.  
+#         '''
+#         try:
+#          user=User.objects.get(username=authenticate_kwargs['username'])
+#          if not user.is_active:
+#              self.error_messages['no_active_account']=(
+#                  'User awaits permission from Admin'
+#              )
+#              raise exceptions.AuthenticationFailed(
+#                  self.error_messages['no_active_account'],
+#                  'no_active_account',
+#              )
+#         except User.DoesNotExist:
+#           self.error_messages['no_active_account'] =(
+#               'Account does not exist')
+#           raise exceptions.AuthenticationFailed(
+#               self.error_messages['no_active_account'],
+#               'no_active_account',
+#           )
+          
+#         '''
+#         We come here if everything above goes well.
+#         Here we authenticate the user.
+#         The authenticate function return None if the credentials do not match 
+#         or the user account is inactive. However here we can safely raise the exception
+#         that the credentials did not match as we do all the checks above this point.
+#         '''
+        
+#         self.user = authenticate(**authenticate_kwargs)
+#         if self.user is None:
+#             self.error_messages['no_active_account'] =(
+#                 'Credentials did not match')
+#             raise exceptions.AuthenticationFailed(
+#                 self.error_messages['no_active_account'],
+#                 'no_active_account',
+#             )
+#         return super().validate(attrs)
+    
+
+
+
+class User_Serializer(serializers.ModelSerializer):
+
+    #all extra user information from profile model
+    # name = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = User
+        fields = [
+                'first_name',
+                'last_name',
+                'email',
+                'username',
+                'is_staff'
+                ]
+        
+    # def get_name(self, obj):
+    #     name = obj.first_name
+    #     if name == '':
+    #         name = obj.username
+
+    #     return name
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    #if token is decoded the followed information would be shown
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+
+        return token
+    
+    #all the json response information for user
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        serializer = UserSerializerWithToken(self.user).data
+        
+        for key, value in serializer.items():
+            data[key] = value
+            
+        
+        return data
+
+class UserSerializerWithToken(User_Serializer):
+
+    token = serializers.SerializerMethodField(read_only = True)
+    #verified = serializers.BooleanField(source = 'profile.verified')
+        
+    class Meta:
+        model = User
+        fields = [
+            'id', 
+            'username', 
+            'email', 
+            'first_name',
+            'last_name',
+            'is_staff', 
+            'token', 
+
+        ]
+    
+    #new access token
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,85 @@
+from argparse import ArgumentDefaultsHelpFormatter
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .serializers import *
+from .models import *
 
-# Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer    
+
+class MyUserView(APIView):
+    #POST user information and add him to database
+    def post(self,request,*args,**kwargs):
+        data = request.data
+        try:
+            if not data.contains("first_name"):
+                return Response("First Name",status=status.HTTP_400_BAD_REQUEST)
+            user = User.objects.create(
+                first_name = data['first_name'],
+                last_name = data['last_name'], 
+                username = data['username'], 
+                email = data['email'],
+                password = make_password(data['password'])
+            )
+#            myuser = MyUser.objects.create(
+#                user = user
+#            )
+            serializer = User_Serializer(user)
+            
+            return Response(serializer.data)
+        except Exception as e: 
+            print(e)
+            message = {'detail' : 'There is already an account using this email or username.'}
+            return Response(message, status=status.HTTP_400_BAD_REQUEST)
+        
+    #GET user info by requested user
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response({"details":"Authentication credentials were not provided."},status=status.HTTP_401_UNAUTHORIZED)
+
+        user = request.user
+        
+
+        serializer = User_Serializer(user)
+        return Response(serializer.data)
+
+
+class LocationsView(APIView):
+    @permission_classes([IsAuthenticated])
+    def get(self, request, *args, **kwargs):
+        #Get search parameters and return locations matching search
+        pass
+    @permission_classes([IsAuthenticated,IsAdminUser])
+    def post(self, request, *args, **kwargs):
+        #Check for admin user, and if so then add file data to database
+        pass
+
+class SingleLocationView(APIView):
+    @permission_classes([IsAuthenticated])
+    def get(self, request, *args, **kwargs):
+        #Get search parameters and return locations matching search
+        pass
+
+
+class SearchView(APIView):
+    @permission_classes([IsAuthenticated])
+    def get(self, request, *args, **kwargs):
+        #Get all searchs that the user is subscribed to
+        pass
+    @permission_classes([IsAuthenticated])
+    def post(self, request, *args, **kwargs):
+        #Post new search
+        pass
+    @permission_classes([IsAuthenticated])
+    def delete(self, request, *args, **kwargs):
+        #Remove search the search from saved searches
+        pass
+
