689d7af79e25041a1bed3b618e35b43a43e0860f
Eleftherios Stefanou <69395008+elefstefanou@users.noreply.github.com>
2023-06-05 23:04:55 +0300

    Search Results Dummy Data [ADD]

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,29 +1,84 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, {useState} from 'react'
+import {GoogleMap, Marker, MarkerF, useJsApiLoader} from '@react-google-maps/api';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
 function Home() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.REACT_APP_MAPS
-  })
+    const {isLoaded} = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.REACT_APP_MAPS
+    })
+
+    const [searchValue, setSearchValue] = useState('');
+    const [points, setPoints] = useState([]);
+
+    const handleSearch = () => {
+        const keywordPointsMap = {
+            Lakes: [
+                {lat: 37.77, lng: -122.41},
+                {lat: 34.05, lng: -118.24}
+            ],
+            // Add more keyword-point mappings here
+        };
+
+        if (searchValue in keywordPointsMap) {
+            setPoints(keywordPointsMap[searchValue]);
+        } else {
+            setPoints([]);
+        }
+    };
+
+    const handleSearchInputChange = (event) => {
+        setSearchValue(event.target.value);
+    };
 
 
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={{
-            width: '100%',
-            height: '100vh'
-          }}
-        center={{
-            lat: -3.745,
-            lng: -38.523
-          }}
-        zoom={10}
-      >
-        <></>
-      </GoogleMap>
-  ) : <></>
+    return isLoaded ? (
+        <div>
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                padding: '10px',
+                backgroundColor: '#8ab4f8'
+            }}>
+                <TextField
+                    type="text"
+                    value={searchValue}
+                    onChange={handleSearchInputChange}
+                    label="Search"
+                    variant="outlined"
+                    style={{marginRight: '10px'}}
+                />
+                <Button variant="contained" color="primary" onClick={handleSearch}>
+                    Search
+                </Button>
+            </div>
+            <GoogleMap
+                id="map"
+                mapContainerStyle={{
+                    width: '100%',
+                    height: '100vh'
+                }}
+                center={points.length > 0 ? points[0] : {lat: 48.8583, lng: 2.2923}}
+                zoom={10}
+                onLoad={(map) => {
+                    let bounds = new window.google.maps.LatLngBounds();
+
+                    points.forEach((point) => {
+                        bounds.extend(point);
+                    });
+
+                    map.fitBounds(bounds);
+                }}
+            >
+                {points.map((point, index) => (
+                    <MarkerF key={index} position={point}/>
+                ))}
+            </GoogleMap>
+        </div>
+    ) : <></>
 }
 
 export default React.memo(Home)
