c1552020ba4cc785147f389beaf38c5da7a42170
NikPnevmatikos <61566326+NikPnevmatikos@users.noreply.github.com>
2023-06-08 10:44:50 +0300

    enviromental variables update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,10 +182,15 @@ CORS_ORIGIN_WHITELIST = [
 
 # CORS_ALLOW_ALL_ORIGINS = True
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = False
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'xplorersgeo@gmail.com'
-EMAIL_HOST_PASSWORD = 'knpthvrznroharlp'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
