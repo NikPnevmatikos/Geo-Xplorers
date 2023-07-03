ea53924e0174769f0010141a7cf31077f7bbbb28
Eleftherios Stefanou <69395008+elefstefanou@users.noreply.github.com>
2023-07-04 00:58:29 +0300

    Categories, htmlCanvas [ADD]

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,39 +1,107 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {GoogleMap, Marker, MarkerF, useJsApiLoader} from '@react-google-maps/api';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import html2canvas from 'html2canvas';
 
 
 function Home() {
     const {isLoaded} = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+        
     })
-
     const [mapInstance, setMapInstance] = useState(null);
     const [searchValue, setSearchValue] = useState('');
     const [points, setPoints] = useState([]);
+    const mapRef = useRef(null);
+    let dataUrl = null;
 
-    const handleSearch = () => {
-        const keywordPointsMap = {
-            Lakes: [
-                {lat: 37.77, lng: -122.41},
-                {lat: 34.05, lng: -118.24},
-                {lat: 57.79, lng: -122.41},
-                {lat: 34.05, lng: 118.24},
-            ],
-            // Add more keyword-point mappings here
+    const [categories, setCategories] = useState([]);
+
+    const handleCategoriesClick = async () => {
+      const response = await fetch("http://localhost:8000/api/search/categories");
+      const data = await response.json();
+      setCategories(data);
+    };
+  
+    const handleCategoryChange = (event) => {
+      const categoryName = event.target.value;
+      const isChecked = event.target.checked;
+      if (isChecked) {
+        setCategories([...categories, categoryName]);
+      } else {
+        setCategories(categories.filter((category) => category !== categoryName));
+      }
+    };
+
+    const handleSearch = async () => {
+        const requestBody = {
+            filters: {
+                categories: [],
+                keywords: [],
+                distance: {},
+            },
+            text: "test",
         };
 
-        if (searchValue in keywordPointsMap) {
-            setPoints(keywordPointsMap[searchValue]);
-        } else {
+        try {
+            console.log("searching");
+            const response = await fetch("http://localhost:8000/api/search/pois/", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(requestBody),
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch points");
+            }
+
+            const data = await response.json();
+            const points = data.map((point) => ({
+                lat: point.latitude,
+                lng: point.longitude,
+            }));
+            setPoints(points);
+        } catch (error) {
+            console.error(error);
             setPoints([]);
         }
     };
 
     const handleSearchInputChange = (event) => {
         setSearchValue(event.target.value);
+    };
+
+
+    function getMap() {
+        const mapElement = document.querySelector('#map-container');
+        html2canvas(mapElement).then(canvas => {
+            dataUrl = canvas.toDataURL();
+            console.log(dataUrl);
+        });
+    }
+    const handleSaveSearch = async () => {
+        getMap();
+        try {
+            const response = await fetch("http://localhost:8000/api/search/savedscreenshot", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({dataUrl}),
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to save screenshot");
+            }
+
+            console.log("Screenshot saved successfully");
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     useEffect(() => {
@@ -61,8 +129,18 @@ function Home() {
                     variant="outlined"
                     style={{marginRight: '10px'}}
                 />
+                <button onClick={handleCategoriesClick}>Categories</button>
+                {categories.map((category) => (
+                <div key={category}>
+                <input type="checkbox" value={category} onChange={handleCategoryChange} />
+                <label>{category}</label>
+                </div>
+      ))}
                 <Button variant="contained" color="primary" onClick={handleSearch}>
                     Search
+                </Button>
+                <Button variant="contained" color="primary" onClick={handleSaveSearch}>
+                    Save Search
                 </Button>
             </div>
             <GoogleMap
@@ -73,14 +151,17 @@ function Home() {
                 }}
                 center={points.length > 0 ? points[0] : {lat: 48.8583, lng: 2.2923}}
                 zoom={10}
-                onLoad={(map) => setMapInstance(map)}
+                onLoad={(map) => {
+                    setMapInstance(map);
+                    mapRef.current = map;
+                }}
             >
                 {points.map((point, index) => (
                     <MarkerF key={index} position={point}/>
                 ))}
             </GoogleMap>
         </div>
-    ) : <></>
+    ) : null;
 }
 
 export default React.memo(Home)
